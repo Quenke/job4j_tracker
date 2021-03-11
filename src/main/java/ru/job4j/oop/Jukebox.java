32bfc57da765c0607f5d51f44a2d7e2d89bc9766
Quenke <1bigstepforward1@gmail.com>
2021-03-11 16:20:05 +0300

    Вызов метода с аргументами.

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+    public static void music(int position) {
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже");
+        } else if (position == 2) {
+            System.out.println("Спокойной ночи");
+        }
+        else if (position != 1 || position != 2) {
+            System.out.println("Песня не найдена");
+        }
+    }
+    public static void main (String [] args){
+            Jukebox position = new Jukebox();
+            String song = "I believe, I can fly";
+            Jukebox.music(1);
+            Jukebox.music(2);
+            Jukebox.music(0);
+            Jukebox.music(105);
+        }
+    }
