4e818eec6bc5094198debc7b83aad6dc636acd72
Quenke <1bigstepforward1@gmail.com>
2021-05-28 15:59:25 +0300

    Даты и время

---
+++ src/main/java/ru/job4j/oop/UsagePackageDate.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+import java.time.LocalDate;
+
+public class UsagePackageDate {
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Текущая дата: " + currentDate);
+    }
+
+}
---
+++ src/main/java/ru/job4j/oop/UsagePackageTime.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+import java.time.LocalTime;
+
+public class UsagePackageTime {
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("Текущее время: " + currentTime);
+    }
+
+}
