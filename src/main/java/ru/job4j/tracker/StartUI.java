d322d1f1ff4713148ee822e00b08f29be4fa986d
Quenke <1bigstepforward1@gmail.com>
2021-05-28 16:29:24 +0300

    Date. Отображение даты.

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,8 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+    public static void main(String[] args) {
+        StartUI item = new StartUI();
+        System.out.println(Item.getCreated);
+    }
+}
