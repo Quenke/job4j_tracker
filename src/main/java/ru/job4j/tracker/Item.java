8e51b018b62fbdb52e709483081ae5a4b0eaa9ab
Quenke <1bigstepforward1@gmail.com>
2021-06-07 20:20:38 +0300

    Date. Отображение даты

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,8 +5,7 @@ import java.time.LocalDateTime;
 public class Item {
     private int id;
     private String name;
-    private LocalDateTime created;
-
+    private LocalDateTime created = LocalDateTime.now();;
     public Item() {
 
     }
