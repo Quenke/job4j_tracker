1c9bdf5b8f103fed7c9f7ed4c3e6c6227390b0b7
Quenke <1bigstepforward1@gmail.com>
2021-06-07 16:07:26 +0300

    Date. Отображение даты

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,15 +1,11 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        /*StartUI localDateTime = new StartUI();*/
-        System.out.println(LocalDateTime.now());
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-        String currentDateTimeFormat = LocalDateTime.now().format(formatter);
-        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+       Item example = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(example.getCreated().format(formatter));
     }
 }
